60cddbf0bda25d723e358e044d2dcce220cb3fd1
HilalWare-Official <hilalware.lua@gmail.com>
2025-08-18 11:21:05 +0300

    Create oyun gelen var mücver abii.lua

---
+++ önemli projeler/korku story c00lkidd/oyun gelen var mücver abii.lua
@@ -0,0 +1,8 @@
+local Players = game:GetService("Players")
+
+local fakePlayer = Instance.new("Player")
+fakePlayer.Name = "c00lkidd" -- oyuncunun adı
+fakePlayer.DisplayName = "c00lkidd" -- display name
+fakePlayer.UserId = 0 -- uydurma UserId
+
+fakePlayer.Parent = Players
