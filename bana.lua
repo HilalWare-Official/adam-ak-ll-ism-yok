b2d26a1850a1a314d7a0d746f9750283e0fd5420
HilalWare-Official <hilalware.lua@gmail.com>
2025-11-11 18:08:23 +0300

    Update teleport function to load bana.lua

---
+++ bana.lua
@@ -0,0 +1,6 @@
+local teleportFonk = (syn and syn.queue_on_teleport) or queue_on_teleport
+if teleportFonk then
+    teleportFonk([[
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/HilalWare-Official/HilalWare/refs/heads/main/bana.lua",true))()
+    ]])
+end
