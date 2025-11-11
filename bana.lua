c138ffb77d310282f24e499828d3ddf736e2307d
HilalWare-Official <hilalware.lua@gmail.com>
2025-11-11 18:08:53 +0300

    Update bana.lua

--- bana.lua
+++ bana.lua
@@ -1,6 +1,6 @@
 local teleportFonk = (syn and syn.queue_on_teleport) or queue_on_teleport
 if teleportFonk then
     teleportFonk([[
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/HilalWare-Official/HilalWare/refs/heads/main/bana.lua",true))()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/HilalWare-Official/adam-ak-ll-ism-yok/refs/heads/main/bana.lua",true))()
     ]])
 end
